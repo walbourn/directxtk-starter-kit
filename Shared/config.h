5e6f3709d0c5407b496a15f86279e9cff6b56900
Chuck Walbourn <chuckw@microsoft.com>
2022-02-25 16:22:26 -0800

    Add BUILD_DX11 option

--- Shared/config.h
+++ Shared/config.h
@@ -4,5 +4,13 @@
 
 #pragma once
 
+#if !defined(BUILD_DX11) && !defined(BUILD_DX12)
+#error You must define BUILD_DX11 or BUILD_DX12
+#endif
+
+#if defined(BUILD_DX11) && defined(BUILD_DX12)
+#error You must define only one of BUILD_DX11 or BUILD_DX12
+#endif
+
 // Default to fullscreen at startup
 //#define DEFAULT_FULLSCREEN
